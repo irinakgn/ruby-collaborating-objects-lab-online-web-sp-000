e0b701fed8d011ab6f04e0e592e7945ef0bac689
Irina Kagan <irinagamb@gmail.com>
2019-05-12 15:08:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,4 +21,27 @@ class Artist
     @@all
   end
 
+  def self.find_or_create_by_name(name)   #class method uses find/create class
+                                        #methods to detect or create instances
+  if self.find(name)
+    self.find(name)
+  else
+    self.create(name)
+  end
+end
+
+def self.find(name)     #class method detects instances from @@all class variable
+  self.all.detect { |artist| artist.name == name }
+end
+
+def self.create(name)   #class method creates & stores instances vs initializing
+  artist = Artist.new(name)
+  artist.save
+  artist
+end
+
+def print_songs #instance method iterates through @songs of an instance of Artist
+  self.songs.each {|song| song.name}
+end
+
 end
