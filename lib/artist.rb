a83fa68021978e09bc8ec4c0d1ed498a093d27ae
Irina Kagan <irinagamb@gmail.com>
2019-05-12 15:07:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,22 @@
 class Artist
+  attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
 
-
-  def initialize(artist)
-    @artists = []
-    @artists << artist
+  def initialize(name)
+    @name = name
+    @songs = []
   end
 
-end
+  def add_song(song)
+    self.songs << song    #Artist.songs reader returns all songs 
+                          #stored in @songs
+  end
+
+  def save
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
+  end
+
+  def self.all      #class method all returns all instances of Artist class 
+    @@all
+  end
